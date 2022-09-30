3b37fa3478ff6bec3cd24088ad9880c35c43c84c
Murilo-MRS <murilosantana1993@gmail.com>
2022-09-30 15:05:53 -0300

    [dev] - Requisito 3 - add input de valor e descricao

--- src/components/WalletForm.jsx
+++ src/components/WalletForm.jsx
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
 
 class WalletForm extends Component {
+  state = {
+    valueInput: 0,
+    description: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, this.verifyInput);
+  };
+
   render() {
+    const { valueInput, description } = this.state;
     return (
-      <div>WalletForm</div>
+      <form>
+        <label htmlFor="value-input">
+          Valor:
+          {' '}
+          <input
+            type="number"
+            name="valueInput"
+            value={ Number(valueInput) }
+            step="0.01"
+            id="value-input"
+            data-testid="value-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="value-input">
+          Descrição:
+          {' '}
+          <input
+            type="text"
+            name="description"
+            value={ description }
+            step="0.01"
+            id="value-input"
+            data-testid="value-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
     );
   }
 }
